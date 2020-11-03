1a2a44658100c5e1f0980990790ed30d16d582f0
p1chkn <p7chkn@yandex.ru>
2020-11-03 15:22:23 +0700

    add parcer

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Ingredient
+import json
 
 
 def index(request):
+    """
+    with open('ingredients.json', 'r') as ingredients:
+        ingredients_json = json.load(ingredients)
+        for ingredient in ingredients_json:
+            Ingredient.objects.create(title=ingredient['title'], dimension=ingredient['dimension'])
+    """
     return render(request, 'indexNotAuth.html')
