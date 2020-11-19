ca54a1dccad9a38b4f811a89b8beb6c440e15c38
p1chkn <p7chkn@yandex.ru>
2020-11-19 10:06:19 +0700

    done changing password, add emulation of email

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -148,3 +148,7 @@ LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
