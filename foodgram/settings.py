6353412870df825d7a8dae8b6fdf51429499602b
p1chkn <p7chkn@yandex.ru>
2020-11-24 11:22:34 +0700

    postrgres

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, datetime, environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
